0abe5eb09e2e37b873038c92f2cc06e95e78b852
Alex Uldahl <alex@uldahl.dk>
2026-09-22 12:03:57 +0200

    Revert "i can finally live my life now"
    This reverts commit 78109fb9a083da2f0ac0b52ff34ab4b2ac51640d.

--- API/Controllers/AuthorsController.Commands.cs
+++ API/Controllers/AuthorsController.Commands.cs
@@ -61,10 +61,13 @@ public partial class AuthorsController
         _ = db.Authors().FirstOrDefault(a => a.Id == request.Id) ??
             throw new KeyNotFoundException("author didnt exist");
 
-        var a = db.Authors().FirstOrDefault(a => a.Id == request.Id) ?? throw new ValidationException();
+        var a = new Author()
+        {
+            FirstName = request.FirstName
+        };
         if (request.Bio != null)
             a.Bio = request.Bio;
-        if (request.FirstName != null)
+        if (request.FirstName!= null)
             a.FirstName = request.FirstName;
         if (request.LastName != null)
             a.LastName = request.LastName;
